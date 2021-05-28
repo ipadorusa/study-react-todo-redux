55b9c5963428034bc6de3122aa513a5da4cc678b
ipadorusa <ipadorusa@gmail.com>
2021-05-28 22:12:11 +0900

    git add

--- src/components/TodoContext.js
+++ src/components/TodoContext.js
@@ -27,6 +27,7 @@ function todoReducer(state, action) {
   switch (action.type) {
     case 'CREATE':
       return state.concat(action.todo)
+
     case 'TOGGLE':
       return state.map((todo) =>
         todo.id === action.id ? { ...todo, done: !todo.done } : todo
